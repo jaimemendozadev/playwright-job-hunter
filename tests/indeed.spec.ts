125512586686366ed405edf38ed966bf1dba7d89
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-02-20 10:03:12 -0800

    Add job what/where search locators

--- tests/indeed.spec.ts
+++ tests/indeed.spec.ts
@@ -1,6 +1,10 @@
 import {test} from '@playwright/test';
 
-
 test('Run indeed.com scraping', async({page}) => {
   await page.goto('https://indeed.com');
-});
+
+  await page.locator("input#text-input-what").type("");
+
+  await page.locator("input#text-input-where").type("");
+
+}); 
