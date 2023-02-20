be0a245f30c9dc3ea1f1b10c70dbbcb70efaaea7
jaimemendozadev <5395811+jaimemendozadev@users.noreply.github.com>
2023-02-20 10:07:37 -0800

    Add .env file to project

--- tests/indeed.spec.ts
+++ tests/indeed.spec.ts
@@ -1,10 +1,11 @@
 import {test} from '@playwright/test';
+import env from '../env';
 
 test('Run indeed.com scraping', async({page}) => {
   await page.goto('https://indeed.com');
 
-  await page.locator("input#text-input-what").type("");
+  await page.locator("input#text-input-what").type(env.searchKeyword);
 
-  await page.locator("input#text-input-where").type("");
+  await page.locator("input#text-input-where").type(env.location[0]);
 
 }); 
